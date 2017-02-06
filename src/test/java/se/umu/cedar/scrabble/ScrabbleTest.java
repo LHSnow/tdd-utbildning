369659c058a3bd38cb7cce309babd69c7345dd31
Lars-Henrik Snow <lars-henrik.snow@umu.se>
2017-02-06 19:52:57 +0100

    lösning på kanarietest

--- src/test/java/se/umu/cedar/scrabble/ScrabbleTest.java
+++ src/test/java/se/umu/cedar/scrabble/ScrabbleTest.java
@@ -2,5 +2,11 @@ package se.umu.cedar.scrabble;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class ScrabbleTest {
+    @Test
+    public void canary() {
+        assertTrue(true);
+    }
 }
