9619d849cbe329dfe6a71ccb123f9c679917d1bf
Lars-Henrik Snow <lars-henrik.snow@umu.se>
2017-02-07 15:32:43 +0100

    refaktorisering: inline av testvariabler

--- src/test/java/se/umu/cedar/scrabble/CombiningWordTest.java
+++ src/test/java/se/umu/cedar/scrabble/CombiningWordTest.java
@@ -20,44 +20,32 @@ public class CombiningWordTest {
             either across or down with one letter on the center square. Diagonal words are not allowed.
         */
     @Test
-    public void combine_letters_to_word() {
-        String withLetters = "ABCDEFG";
-        String word = "BAG";
-        assertTrue(scrabble.allowCombining(word, withLetters));
+    public void one_combines_letters_to_form_a_word() {
+        assertTrue(scrabble.allowCombining("BAG", "ABCDEFG"));
     }
 
     @Test
     public void single_letter_words_are_not_allowed() {
-        String withLetters = "ABCDEFG";
-        String word = "A";
-        assertFalse(scrabble.allowCombining(word, withLetters));
+        assertFalse(scrabble.allowCombining("A", "ABCDEFG"));
     }
 
     @Test
     public void zero_letter_words_are_not_allowed() {
-        String withLetters = "ABCDEFG";
-        String word = "";
-        assertFalse(scrabble.allowCombining(word, withLetters));
+        assertFalse(scrabble.allowCombining("", "ABCDEFG"));
     }
 
     @Test
     public void combining_words_with_non_existent_letters_is_not_allowed() {
-        String withLetters = "ABCDEFG";
-        String word = "BAGS";
-        assertFalse(scrabble.allowCombining(word, withLetters));
+        assertFalse(scrabble.allowCombining("BAGS", "ABCDEFG"));
     }
 
     @Test
     public void using_the_same_letter_twice_is_not_allowed() {
-        String withLetters = "ABCDEFG";
-        String word = "EDGE";
-        assertFalse(scrabble.allowCombining(word, withLetters));
+        assertFalse(scrabble.allowCombining("EDGE", "ABCDEFG"));
     }
 
     @Test
     public void using_two_of_the_same_letter_is_allowed() {
-        String withLetters = "DEEEEEG";
-        String word = "EDGE";
-        assertTrue(scrabble.allowCombining(word, withLetters));
+        assertTrue(scrabble.allowCombining("EDGE", "DEEEEEG"));
     }
 }
