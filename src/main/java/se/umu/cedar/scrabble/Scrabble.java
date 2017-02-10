8d10448ec1a385a7d36eb54528a8ae2cae76ed36
Lars-Henrik Snow <lars-henrik.snow@umu.se>
2017-02-10 12:25:29 +0100

    refaktorisering med hjälpmetoder

--- src/main/java/se/umu/cedar/scrabble/Scrabble.java
+++ src/main/java/se/umu/cedar/scrabble/Scrabble.java
@@ -2,13 +2,22 @@ package se.umu.cedar.scrabble;
 
 public class Scrabble {
     public boolean allowCombining(String word, String availableLetters) {
+        return wordIsMadeUpOfAvailableLetters(word, availableLetters) && wordIsLongEnough(word);
+    }
+
+    private boolean wordIsLongEnough(String word) {
+        return word.length() >= 2;
+    }
+
+    private boolean wordIsMadeUpOfAvailableLetters(String word, String availableLetters) {
         for (char letterInWord : word.toCharArray()) {
             if (availableLetters.indexOf(letterInWord) < 0) {
                 return false;
             } else {
-                availableLetters = availableLetters.replaceFirst("" + letterInWord, "");
+                String letter = String.valueOf(letterInWord);
+                availableLetters = availableLetters.replaceFirst(letter, "");
             }
         }
-        return word.length() >= 2;
+        return true;
     }
 }
