da65d1c6aaa4d15487890162d559324589d51c7f
Lars-Henrik Snow <lars-henrik.snow@umu.se>
2017-02-18 21:03:05 +0100

    Övning 4

--- src/main/java/se/umu/cedar/scrabble/Play.java
+++ src/main/java/se/umu/cedar/scrabble/Play.java
@@ -5,12 +5,18 @@ public class Play {
     private final Reading direction;
     private final int row;
     private final int column;
+    private String player;
 
     public Play(String word, Reading direction, int row, int column) {
         this.word = word;
         this.direction = direction;
         this.row = row;
         this.column = column;
+    }
+
+    public Play(String player, String word, Reading direction, int row, int column) {
+        this(word,direction,row,column);
+        this.player = player;
     }
 
     public String getWord() {
